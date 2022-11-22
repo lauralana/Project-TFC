aa71b225a5ee47dde6c3daa1258d994d52493da2
Laura Lana <lauramarqueslana@gmail.com>
2022-11-22 17:14:36 -0300

    cria pasta de rotas

---
+++ app/backend/src/routes/routes.ts
@@ -0,0 +1,13 @@
+import * as express from 'express';
+
+// rotas do controller
+// import product from '../controllers/productController';
+// import user from '../controllers/userController';
+
+// rota com as funções de validação
+
+const route = express.Router();
+
+// rota dos verbos
+
+export default route;
